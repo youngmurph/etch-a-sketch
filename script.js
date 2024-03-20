893eaec71d2bdb3eb82dda9c491364e3dfb2135f
Conor Murphy <conormurphy@gmx.de>
2024-03-20 11:23:37 -0400

    Create user input function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let grid = document.createElement('div');
 let header = document.createElement('h1');
-let body = document.querySelector('body');
 header.textContent = `Conor's Etch-a-Sketch`;
+let body = document.querySelector('body');
+
+let button = document.createElement('button');
+button.textContent = 'Click here to change grid';
+button.className = 'btn';
+let userChoice = button.addEventListener('click', () => {
+    let choice = prompt('How many rows and columns would you like?');
+    return choice;
+})
+
 body.appendChild(header);
+body.appendChild(button);
 body.appendChild(grid);
 grid.className = 'container';
 
@@ -10,13 +20,28 @@ let rows = 16;
 let columns = 16;
 
 for (let i = 0; i < rows; i++) {
-    const gridRow = document.createElement('div');
+    let gridRow = document.createElement('div');
     gridRow.className = 'grid-row';
     grid.appendChild(gridRow);
     for (let j = 0; j < columns; j++) {
-        const gridColumn = document.createElement('div');
+        let gridColumn = document.createElement('div');
         gridColumn.className = 'grid-column';
         grid.appendChild(gridColumn);
     }
 }
+
+function userChange() {
+    for (let i = 0; i < userChoice; i++) {
+        let gridRow = document.createElement('div');
+        gridRow.className = 'grid-row';
+        grid.appendChild(gridRow);
+        for (let j = 0; j < userChoice; j++) {
+            let gridColumn = document.createElement('div');
+            gridColumn.className = 'grid-column';
+            grid.appendChild(gridColumn);
+        }
+    }
+}
+
+userChange();
 
